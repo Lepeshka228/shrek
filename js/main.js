b9ecc86e57b08d4878e32923bbd1b5f762cb7709
Lepeshka228 <113051977+Lepeshka228@users.noreply.github.com>
2023-08-23 00:33:25 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-var swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".swiper", {
     pagination: {
       el: ".swiper-pagination",
     },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    effect: "fade",
 });
-
